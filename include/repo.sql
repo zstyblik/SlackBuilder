d1af385dee7b26f0133dd72662f3f029ea0b5a6e
Zdenek Styblik <stybla@turnovfree.net>
2012-02-07 06:25:45 +0100

    MOD include/repo.sql - doc/example, new column

--- include/repo.sql
+++ include/repo.sql
@@ -2,13 +2,18 @@
 --  appl: wine
 --  version: 1.3.7
 --  name: wine-1.3.7-x86_64-1alien
---  path: slackware64/xap/wine-1.3.7-x86_64-1alien
+--  suffix: .txz
+--  repo_path: slackware64/xap/
 --  checksum: MD5#5483e192e6fbdc95c8eaf9ed46d61e70 -> ALGO:HASH
---
+-- 
+-- Package, all versions: repo_path + appl
+-- Package, zero-in: repo_path + name
+-- Package with different suffix is still treated as the same file!
 CREATE TABLE repo (
 	appl TEXT,
 	version TEXT,
-	name TEXT, 
-	repo_path TEXT, 
+	name TEXT,
+	suffix TEXT,
+	repo_path TEXT,
 	checksum TEXT
 );
