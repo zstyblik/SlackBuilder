432bb71a31d4fe94d6bde7b536dc37a4958aee73
Zdenek Styblik <stybla@turnovfree.net>
2012-02-08 06:14:27 +0100

    FIX repo.sh - PKG not in DB only if PKG_COUNT == 0 Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>
    Print PKG is not in DB only if PKGFOUND_COUNT is 0. If PKGFOUND_COUNT is eg. 2,
    then PKG is in DB for sure.

--- repo.sh
+++ repo.sh
@@ -227,7 +227,9 @@ repo_delete() {
 	PKGFOUND_COUNT=$(sqlite3 "${SQL_DB}" "SELECT COUNT(appl) FROM repo WHERE \
 		repo_path = '${SQL_REPO_PATH}';")
 	if [ "${PKGFOUND_COUNT}" != "1" ] && [ $FORCE -eq 0 ]; then
-		printf "repo_delete(): Package not found in DB.\n" 1>&2
+		if [ "${PKGFOUND_COUNT}" == "0" ]; then
+			printf "repo_delete(): Package not found in DB.\n" 1>&2
+		fi
 		printf "repo_delete(): PKGs found %s, expected 1.\n" \
 			"${PKGFOUND_COUNT}" 1>&2
 		printf "repo_delete(): Perhaps you want to use force.\n" 1>&2
